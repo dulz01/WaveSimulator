a6a3d911373e1e2b1acd74718c49ac727547d852
Abdullah Mukhtar Mansur Bin Abdullah <yuureijin@gmail.com>
2017-03-19 23:50:59 +0000

    Constrained the points range to the screen coordinates.

--- octet/src/examples/example_waves/water_surface.h
+++ octet/src/examples/example_waves/water_surface.h
@@ -6,8 +6,22 @@ namespace octet {
   
   public:
     std::vector<vec3> generate_water_surface(int x, int y) {
+      float tempX = 0.0f;
+      float tempY = 0.0f;
+      int maxRangeX = 0;
+      int maxRangeY = 0;
+      int screenRange = 1 - (-1);
+
       for (int i = 0; i < (x*y); i++) {
-        points.push_back(vec3(i % x, i / x, 0));
+        tempX = i % x;
+        tempY = i / x;
+        maxRangeX = x - 1;
+        maxRangeY = y - 1;
+
+        tempX = (maxRangeX * -1 - 0 * 1) / maxRangeX + tempX * screenRange / maxRangeX;
+        tempY = (maxRangeY * -1 - 0 * 1) / maxRangeY + tempY * screenRange / maxRangeY;
+
+        points.push_back(vec3(tempX, tempY, 0));
       }
 
       return points;
