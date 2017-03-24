cffb74fefceae558b5af7e0eabb74e67d1bf487b
Abdullah Mukhtar Mansur Bin Abdullah <yuureijin@gmail.com>
2017-03-24 13:03:08 +0000

    Implemented a rough mesh code from example_geometry project in octet.

--- octet/src/examples/example_waves/example_waves.h
+++ octet/src/examples/example_waves/example_waves.h
@@ -52,11 +52,13 @@ namespace octet {
       app_scene =  new visual_scene();
       app_scene->create_default_camera_and_lights();
 
-      material *red = new material(vec4(1, 0, 0, 1));
-      mesh_box *box = new mesh_box(vec3(4));
+      material *red = new material(vec4(0, 0, 1, 1));
       scene_node *node = new scene_node();
       app_scene->add_child(node);
-      app_scene->add_mesh_instance(new mesh_instance(node, box, red));
+      //app_scene->add_mesh_instance(new mesh_instance(node, box, red));
+      water_surface w;
+      mesh *water = w.Get_Mesh();
+      app_scene->add_mesh_instance(new mesh_instance(node, water, red));
     }
 
     /// this is called to draw the world
@@ -74,8 +76,8 @@ namespace octet {
 
       // tumble the box  (there is only one mesh instance)
       scene_node *node = app_scene->get_mesh_instance(0)->get_node();
-      node->rotate(1, vec3(1, 0, 0));
-      node->rotate(1, vec3(0, 1, 0));
+      //node->rotate(1, vec3(1, 0, 0));
+      //node->rotate(1, vec3(0, 1, 0));
     }
   };
 }
--- octet/src/examples/example_waves/water_surface.h
+++ octet/src/examples/example_waves/water_surface.h
@@ -2,30 +2,81 @@
 
 namespace octet {
   class water_surface {
-    std::vector<vec3> points;
-  
-  public:
-    std::vector<vec3> generate_water_surface(int x, int y) {
-      float tempX = 0.0f;
-      float tempY = 0.0f;
-      int maxRangeX = 0;
-      int maxRangeY = 0;
-      int screenRange = 1 - (-1);
+    struct my_vertex {
+      vec3p pos;
+      uint32_t color;
+    };
 
-      for (int i = 0; i < (x*y); i++) {
-        tempX = i % x;
-        tempY = i / x;
-        maxRangeX = x - 1;
-        maxRangeY = y - 1;
+    ref<mesh> water_mesh;
+    std::vector<vec3> water_plane;
 
-        // converting to screen space range
-        tempX = (maxRangeX * -1 - 0 * 1) / maxRangeX + tempX * screenRange / maxRangeX;
-        tempY = (maxRangeY * -1 - 0 * 1) / maxRangeY + tempY * screenRange / maxRangeY;
 
-        points.push_back(vec3(tempX, tempY, 0));
+    void init() {
+      water_mesh = new mesh();
+
+      water_plane.resize(10000);
+
+      // allocate vertices and indices into OpenGL buffers
+      size_t num_steps = 320;
+      size_t num_indices = num_steps * 6;
+      water_mesh->allocate(sizeof(my_vertex) * water_plane.size(), sizeof(uint32_t) * num_indices);
+      water_mesh->set_params(sizeof(my_vertex), num_indices, water_plane.size(), GL_TRIANGLES, GL_UNSIGNED_INT);
+      
+      // describe the structure of my_vertex to OpenGL
+      water_mesh->add_attribute(attribute_pos, 3, GL_FLOAT, 0);
+      water_mesh->add_attribute(attribute_color, 4, GL_UNSIGNED_BYTE, 12, GL_TRUE);
+
+      {
+        // these write-only locks give access to the vertices and indices.
+        // they will be released at the next } (the end of the scope)
+        gl_resource::wolock vl(water_mesh->get_vertices());
+        my_vertex *vtx = (my_vertex *)vl.u8();
+        gl_resource::wolock il(water_mesh->get_indices());
+        uint32_t *idx = il.u32();
+
+        // make the vertices
+        float radius1 = 1.0f;
+        float radius2 = 7.0f;
+        float height = 24.0f;
+        float num_twists = 4.0f;
+        for (size_t i = 0; i != num_steps + 1; ++i) {
+          float y = i * (height / num_steps) - height * 0.5f;
+          float angle = i * (num_twists * 2.0f * 3.14159265f / num_steps);
+          vtx->pos = vec3p(cosf(angle) * radius1, y, sinf(angle) * radius1);
+          vtx++;
+          vtx->pos = vec3p(cosf(angle) * radius2, y, sinf(angle) * radius2);
+          vtx++;
+        }
+
+        // make the triangles
+        uint32_t vn = 0;
+        for (size_t i = 0; i != num_steps; ++i) {
+          // 0--2
+          // | \|
+          // 1--3
+          idx[0] = vn + 0;
+          idx[1] = vn + 3;
+          idx[2] = vn + 1;
+          idx += 3;
+
+          idx[0] = vn + 0;
+          idx[1] = vn + 2;
+          idx[2] = vn + 3;
+          idx += 3;
+
+          vn += 2;
+        }
       }
-
-      return points;
     }
+
+  public:
+    water_surface() {
+      init();
+    }
+
+    ref<mesh> Get_Mesh() {
+      return water_mesh;
+    }
+
   };
 }
